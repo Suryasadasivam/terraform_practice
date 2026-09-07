4d4079d78b66498ac76083a15bfb61f172e86948
Suryasadasivam <suryasadasivamm@gmail.com>
2026-09-07 11:44:32 +0530

    output file

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,3 @@
-# output "vpc_id" {
-#     value = aws_vpc.shared_vpc.id
-  
-# }
-
-# output "vpc_name" {
-#     value = aws_vpc.shared_vpc.tags["Name"]
-  
-# }
-
-# output "subnet_id"{
-#     value=aws_subnet.shared_subnet.id
-# }
-
-# output "subnet_name"{
-#     value=aws_subnet.shared_subnet.tags["Name"]
-# }
-
-output "instance_id" {
-  value = aws_instance.ec2_main.id
+output "snapshot_id" {
+  value = aws_ebs_snapshot.example_snapshot.id
 }
